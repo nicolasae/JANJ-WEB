c9b471083c968bd854dd5e407e9c5e9737b240c5
nicolasae <nicolas.aguirre@utp.edu.co>
2021-03-06 19:34:26 -0500

    wip

--- janj-proyect/src/Components/about/About.js
+++ janj-proyect/src/Components/about/About.js
@@ -4,26 +4,27 @@ import '../../styles/about/About.css'
 
 function About() {
     return (
-        <div>
-            <h1>Testimonios</h1>
-                <div className="container">
-                    <img src="profile-user.png" />   
-                    <span>Chris Fox.</span> 
-                    <p>John Doe saved us from a web disaster.</p>
-                </div>  
-                <div className="container">
-                    <img src="profile-user.png" />   
-                    <span>Chris Fox.</span> 
-                    <p>John Doe saved us from a web disaster.</p>
-                </div>
-                <div className="container">
-                    <img src="profile-user.png" />   
-                    <span>Chris Fox.</span> 
-                    <p>John Doe saved us from a web disaster.</p>
-                </div>  
-            
-        </div>
-  
+        <section className="about">
+            <div className="about">
+                <h1>Testimonios</h1>
+                    <div className="container-about">
+                        <img src="profile-user.png" />   
+                        <span>Chris Fox.</span> 
+                        <p>John Doe saved us from a web disaster.</p>
+                    </div>  
+                    <div className="container-about">
+                        <img src="profile-user.png" />   
+                        <span>Chris Fox.</span> 
+                        <p>John Doe saved us from a web disaster.</p>
+                    </div>
+                    <div className="container-about">
+                        <img src="profile-user.png" />   
+                        <span>Chris Fox.</span> 
+                        <p>John Doe saved us from a web disaster.</p>
+                    </div>  
+                
+            </div>
+        </section>
 
     );
   }
