8c81b8614bc282b1008f2ec86c8a221a8adc5528
nicolas aguirre <nicolas.aguirre@utp.edu.co>
2021-04-18 21:19:15 -0500

    wip conversor

--- janj-proyect/src/Components/pages/Converter.js
+++ janj-proyect/src/Components/pages/Converter.js
@@ -14,6 +14,7 @@ class Converter extends React.Component{
           hacia: '',
           cantidad: ''
         },
+        valor_convertido:"",
       }
     }
 
@@ -32,10 +33,13 @@ class Converter extends React.Component{
 
       const conversor=()=>{
         var date = new Date()
-        console.log(this.state.form);
+        console.log(date);
         (async () => {
-          let amount = await convert(2000, 'USD', 'EUR', '2018-01-01');
+          let amount = await convert(Number(this.state.form.cantidad), String(this.state.form.desde), String(this.state.form.hacia),'2021-04-18' );
+          // '2018-01-01'
+          this.setState({valor_convertido:amount})
           console.log(amount);    // 1667.6394564000002
+          
         })();
       }
 
@@ -62,81 +66,81 @@ class Converter extends React.Component{
                       <div className="row">
                         <div className="col-lg-6 text-center ">
                           <h2>De:</h2>
-                          <select className="" onChange = {this.handleChange} >
-                            <option selected value="USD" name="desde">Dólar Estadounidense</option>
-                            <option value="AUD" name="desde">Dólar Australiano</option>
-                            <option value="BRL" name="desde">Real Brazileño</option>
-                            <option value="GBP" name="desde">Libra Esterlina</option>
-                            <option value="BGN" name="desde">Leva Búlgaro</option>
-                            <option value="CAD" name="desde">Dólar Canadiense</option>
-                            <option value="CNY" name="desde">Yuan</option>
-                            <option value="HRK" name="desde">Kuna Croata</option>
-                            <option value="CZK" name="desde">Corona Checa</option>
-                            <option value="DKK" name="desde">Corona Danesa</option>
-                            <option value="EUR" name="desde">Euro</option>
-                            <option value="HKD" name="desde">Dólar Hongkonés</option>
-                            <option value="HUF" name="desde">Forinto</option>
-                            <option value="ISK" name="desde">Corona Islandesa</option>
-                            <option value="IDR" name="desde">Rupia Indonesia</option>
-                            <option value="INR" name="desde">Rupia India</option>
-                            <option value="ILS" name="desde">Nuevo Séquel Israelí</option>
-                            <option value="JPY" name="desde">Yen</option>
-                            <option value="MYR" name="desde">Ringit</option>
-                            <option value="MXN" name="desde">Peso Mexicano</option>
-                            <option value="NZD" name="desde">Dólar Neozelandés</option>
-                            <option value="NOK" name="desde">Corona Noruega</option>
-                            <option value="PHP" name="desde">Peso Filipino</option>
-                            <option value="PLN" name="desde">Esloti</option>
-                            <option value="RON" name="desde">Leu Rumano</option>
-                            <option value="RUV" name="desde">Rublo Ruso</option>
-                            <option value="SGD" name="desde">Dólar Singapurense</option>
-                            <option value="ZAR" name="desde">Rand</option>
-                            <option value="KRW" name="desde">Won Surcoreano</option>
-                            <option value="SEK" name="desde">Corona Sueca</option>
-                            <option value="CHF" name="desde">Franco Suizo</option>
-                            <option value="THB" name="desde">Bat</option>
-                            <option value="TRY" name="desde">Libra Turca </option>
-                            <option value="USD" name="desde">Dólar Estadounidense</option>                        
+                          <select className="" onChange = {this.handleChange} name="desde" >
+                            <option selected value="USD">Dólar Estadounidense</option>
+                            <option value="AUD">Dólar Australiano</option>
+                            <option value="BRL">Real Brazileño</option>
+                            <option value="GBP">Libra Esterlina</option>
+                            <option value="BGN">Leva Búlgaro</option>
+                            <option value="CAD">Dólar Canadiense</option>
+                            <option value="CNY">Yuan</option>
+                            <option value="HRK">Kuna Croata</option>
+                            <option value="CZK">Corona Checa</option>
+                            <option value="DKK">Corona Danesa</option>
+                            <option value="EUR">Euro</option>
+                            <option value="HKD">Dólar Hongkonés</option>
+                            <option value="HUF">Forinto</option>
+                            <option value="ISK">Corona Islandesa</option>
+                            <option value="IDR">Rupia Indonesia</option>
+                            <option value="INR">Rupia India</option>
+                            <option value="ILS">Nuevo Séquel Israelí</option>
+                            <option value="JPY">Yen</option>
+                            <option value="MYR">Ringit</option>
+                            <option value="MXN">Peso Mexicano</option>
+                            <option value="NZD">Dólar Neozelandés</option>
+                            <option value="NOK">Corona Noruega</option>
+                            <option value="PHP">Peso Filipino</option>
+                            <option value="PLN">Esloti</option>
+                            <option value="RON">Leu Rumano</option>
+                            <option value="RUV">Rublo Ruso</option>
+                            <option value="SGD">Dólar Singapurense</option>
+                            <option value="ZAR">Rand</option>
+                            <option value="KRW">Won Surcoreano</option>
+                            <option value="SEK">Corona Sueca</option>
+                            <option value="CHF">Franco Suizo</option>
+                            <option value="THB">Bat</option>
+                            <option value="TRY">Libra Turca </option>
+                            <option value="USD">Dólar Estadounidense</option>                        
                           </select>
                         </div>
 
                       <div className="col-lg-6 text-center">
                         <h2>A:</h2>
-                        <select className="" onChange = {this.handleChange}>
-                          <option selected value="USD" name="hacia">Dólar Estadounidense</option>
-                            <option value="AUD" name="hacia">Dólar Australiano</option>
-                            <option value="BRL" name="hacia">Real Brazileño</option>
-                            <option value="GBP" name="hacia">Libra Esterlina</option>
-                            <option value="BGN" name="hacia">Leva Búlgaro</option>
-                            <option value="CAD" name="hacia">Dólar Canadiense</option>
-                            <option value="CNY" name="hacia">Yuan</option>
-                            <option value="HRK" name="hacia">Kuna Croata</option>
-                            <option value="CZK" name="hacia">Corona Checa</option>
-                            <option value="DKK" name="hacia">Corona Danesa</option>
-                            <option value="EUR" name="hacia">Euro</option>
-                            <option value="HKD" name="hacia">Dólar Hongkonés</option>
-                            <option value="HUF" name="hacia">Forinto</option>
-                            <option value="ISK" name="hacia">Corona Islandesa</option>
-                            <option value="IDR" name="hacia">Rupia Indonesia</option>
-                            <option value="INR" name="hacia">Rupia India</option>
-                            <option value="ILS" name="hacia">Nuevo Séquel Israelí</option>
-                            <option value="JPY" name="hacia">Yen</option>
-                            <option value="MYR" name="hacia">Ringit</option>
-                            <option value="MXN" name="hacia">Peso Mexicano</option>
-                            <option value="NZD" name="hacia">Dólar Neozelandés</option>
-                            <option value="NOK" name="hacia">Corona Noruega</option>
-                            <option value="PHP" name="hacia">Peso Filipino</option>
-                            <option value="PLN" name="hacia">Esloti</option>
-                            <option value="RON" name="hacia">Leu Rumano</option>
-                            <option value="RUV" name="hacia">Rublo Ruso</option>
-                            <option value="SGD" name="hacia">Dólar Singapurense</option>
-                            <option value="ZAR" name="hacia">Rand</option>
-                            <option value="KRW" name="hacia">Won Surcoreano</option>
-                            <option value="SEK" name="hacia">Corona Sueca</option>
-                            <option value="CHF" name="hacia">Franco Suizo</option>
-                            <option value="THB" name="hacia">Bat</option>
-                            <option value="TRY" name="hacia">Libra Turca </option>
-                            <option value="USD" name="hacia">Dólar Estadounidense</option>     
+                        <select className="" onChange = {this.handleChange} name="hacia">
+                          <option selected value="EUR" >Euro</option>
+                            <option value="AUD">Dólar Australiano</option>
+                            <option value="BRL">Real Brazileño</option>
+                            <option value="GBP">Libra Esterlina</option>
+                            <option value="BGN">Leva Búlgaro</option>
+                            <option value="CAD">Dólar Canadiense</option>
+                            <option value="CNY">Yuan</option>
+                            <option value="HRK">Kuna Croata</option>
+                            <option value="CZK">Corona Checa</option>
+                            <option value="DKK">Corona Danesa</option>
+                            <option value="EUR">Euro</option>
+                            <option value="HKD">Dólar Hongkonés</option>
+                            <option value="HUF">Forinto</option>
+                            <option value="ISK">Corona Islandesa</option>
+                            <option value="IDR">Rupia Indonesia</option>
+                            <option value="INR">Rupia India</option>
+                            <option value="ILS">Nuevo Séquel Israelí</option>
+                            <option value="JPY">Yen</option>
+                            <option value="MYR">Ringit</option>
+                            <option value="MXN">Peso Mexicano</option>
+                            <option value="NZD">Dólar Neozelandés</option>
+                            <option value="NOK">Corona Noruega</option>
+                            <option value="PHP">Peso Filipino</option>
+                            <option value="PLN">Esloti</option>
+                            <option value="RON">Leu Rumano</option>
+                            <option value="RUV">Rublo Ruso</option>
+                            <option value="SGD">Dólar Singapurense</option>
+                            <option value="ZAR">Rand</option>
+                            <option value="KRW">Won Surcoreano</option>
+                            <option value="SEK">Corona Sueca</option>
+                            <option value="CHF">Franco Suizo</option>
+                            <option value="THB">Bat</option>
+                            <option value="TRY">Libra Turca </option>
+                            <option value="USD">Dólar Estadounidense</option>     
                         </select>
                       </div>
                     </div>
@@ -144,7 +148,8 @@ class Converter extends React.Component{
                         <button className="btn-get-started" onClick={conversor}>Convertir</button>
                       </div>
                       <div>
-                        <h2>El valor es:</h2>
+                        <h2>El valor es: {this.state.valor_convertido}</h2>
+
 
                       </div>
                   </div>  
