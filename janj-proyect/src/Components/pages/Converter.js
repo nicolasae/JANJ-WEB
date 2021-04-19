2bfd056b26c5f7d31bd14f7edd61af23238aa4c6
nicolas aguirre <nicolas.aguirre@utp.edu.co>
2021-04-18 19:55:24 -0500

    wip

--- janj-proyect/src/Components/pages/Converter.js
+++ janj-proyect/src/Components/pages/Converter.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Navbar from '../navbar/Navbar';
 import '../../assets/css/style.css'
+import CurrencyConverter from 'react-currency-conv';
 
 
-class Converter extends Component{
+class Converter extends React.Component{
     
     constructor(props){
       super(props);
@@ -14,46 +15,76 @@ class Converter extends Component{
 
     render(){
       return (
-        <section id="converter">
-          
+        <section id="converter">          
           <div>
           <Navbar/> 
           </div>      
-            <div className="container">
-                      
-              <div className="section-header">
-                <h1 >Convertidor</h1>
-              </div>
-              <div className="row" >
-
-                <div className="col-lg-4 ">
-                  <h2>Cantidad a Convertir</h2>
-                  <input placeholder="Ingrese cantidad a convertir"></input>
-                </div>
-
-                <div className="col-lg-4 ">
-                <h2>De:</h2>
-                <select className="" >
-                  <option selected>Open this select menu</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
-                </div>
-
-                <div className="col-lg-4 ">
-                  <h2>A:</h2>
-                  <select className="" >
-                    <option selected>Open this select menu</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
-                  </select>
+            <div className="container">                  
+                <div className="section-header">
+                  <h1 >Conversor</h1>
                 </div>
               </div>
-            
-          
-          </div>
+              <div className="container">
+                  
+                  <div className="row">
+                    {/* PARTE DE CONVERSIÓN */}
+                    <div className="col-lg-6 text-center">
+                      <div className="col-lg-12 ">
+                        <h2>Cantidad a Convertir:</h2>
+                      </div> 
+
+                      <input placeholder="Ingrese cantidad a convertir"></input>
+                      <div className="row">
+                        <div className="col-lg-6 text-center ">
+                          <h2>De:</h2>
+                          <select className="" >
+                            <option selected>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                          </select>
+                        </div>
+
+                      <div className="col-lg-6 text-center">
+                        <h2>A:</h2>
+                        <select className="" >
+                          <option selected>Open this select menu</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </select>
+                      </div>
+                    </div>
+                      <div className="col-lg-12 text-center">
+                        <button className="btn-get-started">Convertir</button>
+                      </div>
+                  </div>  
+              
+                  {/* PARTE DE INSTRUCCIONES */}
+                  <div className="col-lg-6 ">
+                    <div className="col-lg-12 text-center">
+                      <h3>¿Cómo utilizar el conversor?</h3>
+                    </div>
+                    <ol>
+                      <li>Ingrese la cantidad a <b>convertir</b></li>
+                      <li>Seleccione en <b>“Desde la divisa”</b> la moneda que representa la cantidad a convertir</li>
+                      <li>Seleccione en <b>“A la divisa” la moneda a la cual desea covertir la cantidad ingresada</b></li>
+                      <li>Presione el botón <b>"Convertir"</b></li>
+                    </ol>
+                  </div>
+
+                </div>
+                
+                
+                
+
+              </div>
+
+              
+              <div className="col-12 col-md-6">
+
+
+            </div>         
         </section>
       );
     }
