c4207cc658902bd358d7deedcc8d5eeb07be2b8d
Juan Diego <jcastano@dataifx.com>
2021-04-26 17:15:12 -0500

    updated flask config

---
+++ api/README.md
@@ -0,0 +1,8 @@
+## First create virtualenv:
+virtualenv venv
+## Activate venv:
+source venv/bin/activate
+## Install dependencies:
+pip install -r requirements.txt
+## To run the app, in /api:
+flask run
--- api/api.py
+++ api/api.py
@@ -1,19 +1,24 @@
-from flask import Flask, request, make_response, redirect, render_template,jsonify
-from flask_cors import CORS
-# initialization
+from flask import Flask
 app = Flask(__name__)
-cors = CORS(app)
 
 
-@app.route("/prueba", methods=['GET'])
-def prueba():
-    response= jsonify({'some': 'data'})
-    return response
+@app.route('/')
+def hello():
+    return "Hello World!"
 
-@app.after_request
-def after_request(response):
-    response.headers["Access-Control-Allow-Origin"] = "*" 
-    response.headers["Access-Control-Allow-Credentials"] = "false"
-    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
-    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
-    return response
+@app.route('/login')
+def hello():
+    return "Hello World!"
+
+@app.route('/register')
+def hello():
+    return "Hello World!"
+
+@app.route("/<name>")              # at the end point /<name>
+def hello_name(name):              # call method hello_name
+    return "Hello " + name          # which returns "hello + name
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
