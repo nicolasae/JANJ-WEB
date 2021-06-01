bc9c6d899f24857e73645afd5fde2a30e16b32c4
alejandro <alejandro.vargas@utp.edu.co>
2021-06-01 14:27:20 -0500

    Con modulo de prediccion

--- back/aplicacion/app.py
+++ back/aplicacion/app.py
@@ -317,82 +317,14 @@ def signup_post():
 
 
 
-@app.route("/prediccion", method=["POST"])
+@app.route("/prediccion", methods=["POST"])
 def prediccion():
-    import numpy as np
-    np.random.seed(4)
-    import matplotlib.pyplot as plt
-    import pandas as pd
-    import pandas_datareader as web
-    import datetime as dt
-
-    from sklearn.preprocessing import MinMaxScaler
-    from keras.models import Sequential
-    from keras.layers import Dense, LSTM
-    import requests
-
+    from aplicacion.prediccion import prediccion_acciones
 
     datos = request.get_json()
     ticket = datos.get('ticket')
 
-    start = dt.datetime(2012,1,1)
-    end = dt.datetime(2020,12,31)
-
-    dataset = web.DataReader(company,'yahoo',start,end) #el yahoo es para usar la api de yahoo
-
-    set_entrenamiento = dataset[:'2019'].iloc[:,1:2]
-    set_validacion = dataset['2020':].iloc[:,1:2]
-
-    # Normalización del set de entrenamiento
-    sc = MinMaxScaler(feature_range=(0,1))
-    set_entrenamiento_escalado = sc.fit_transform(set_entrenamiento)
-
-    # La red LSTM tendrá como entrada "time_step" datos consecutivos, y como salida 1 dato (la predicción a
-    # partir de esos "time_step" datos). Se conformará de esta forma el set de entrenamiento
-    time_step = 90
-    X_train = []
-    Y_train = []
-    m = len(set_entrenamiento_escalado)
-
-    for i in range(time_step,m):
-        # X: bloques de "time_step" datos: 0-time_step, 1-time_step+1, 2-time_step+2, etc
-        X_train.append(set_entrenamiento_escalado[i-time_step:i,0])
-
-        # Y: el siguiente dato
-        Y_train.append(set_entrenamiento_escalado[i,0])
-    X_train, Y_train = np.array(X_train), np.array(Y_train)
-
-    # Reshape X_train para que se ajuste al modelo en Keras
-    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-
-    #
-    # Red LSTM
-    #
-    dim_entrada = (X_train.shape[1],1)
-    dim_salida = 1
-    na = 50
-
-    modelo = Sequential()
-    modelo.add(LSTM(units=na, input_shape=dim_entrada))
-    modelo.add(Dense(units=dim_salida))
-    modelo.compile(optimizer='rmsprop', loss='mse')
-    modelo.fit(X_train,Y_train,epochs=10,batch_size=32)
-
-
-    #
-    # Validación (predicción del valor de las acciones)
-    #
-    x_test = set_validacion.values
-    x_test = sc.transform(x_test)
-
-    X_test = []
-    for i in range(time_step,len(x_test)):
-        X_test.append(x_test[i-time_step:i,0])
-    X_test = np.array(X_test)
-    X_test = np.reshape(X_test, (X_test.shape[0],X_test.shape[1],1))
-
-    prediccion = modelo.predict(X_test)
-    prediccion = sc.inverse_transform(prediccion)
+    prediccion = prediccion_acciones(ticket)
 
     return jsonify(prediccion)
 
