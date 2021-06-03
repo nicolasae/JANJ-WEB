9668e30ce9c35b09cc50a3a17babc33483e36e4a
Juan Diego <jcastano@dataifx.com>
2021-06-02 22:06:53 -0500

    implemented email

--- back/aplicacion/app.py
+++ back/aplicacion/app.py
@@ -13,10 +13,36 @@ import random
 import requests
 import math
 
+from flask_mail import Mail, Message
+from credentials import Credentials
+
+template = ''' <!DOCTYPE html>
+                <html>
+                <body>
+
+                <h1 style="color:#ff471a;">Su acción en JANJ-WEB tiene buenas predicciones</h1>
+                <p>El sistema de suscripción le informa que la acción de: </p> 
+                <p style="color:#0000FF; font-weight: bold;">ECOPETROL </p>
+
+                </body>
+                </html> '''
+
 app = Flask(__name__)
+credentials = Credentials()
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": credentials.get_user(),
+    "MAIL_PASSWORD": credentials.get_password()
+}
 app.config.from_object(config)
+app.config.update(mail_settings)
 Bootstrap(app)
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 app.config["JWT_SECRET_KEY"] = "A0Zr98j/3yX R~XLH!tmN]LWk/,?RT"
 
@@ -40,6 +66,21 @@ def user_lookup_callback(_jwt_header, jwt_data):
 def inicial():
     return jsonify("pagina inicial")
 
+@app.route("/send_email", methods=['POST'])
+def send_email():
+    print(request.get_json(force=True))
+    emails = list(request.get_json(force=True)["emails"])
+    print(emails, type(emails))
+    with app.app_context():
+        msg = Message(subject="ATENCIÓN: Su acción en JANJ tiene buenas proyecciones",
+                    sender=app.config.get("MAIL_USERNAME"),
+                    recipients=emails, # use your email for testing
+        )
+        msg.html = template
+        # print(render_template(template+'.txt', **kwargs))
+        # msg.html = render_template(template+'.html', **kwargs)
+        mail.send(msg)
+        return "Se envió email correctamente"
 
 @app.route("/back/login", methods=["POST"])
 def login():
--- back/aplicacion/config.py
+++ back/aplicacion/config.py
@@ -6,3 +6,5 @@ PWD = os.path.abspath(os.curdir)
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+EMAIL_USER = "web.janj@gmail.com"
+EMAIL_PASSWORD = "Ingesoft4@"
